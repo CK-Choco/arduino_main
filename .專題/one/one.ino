dc74f318bcf1fa3b154552f519461ad71d00215f
HsiaoChoco <avatyu89@gmail.com>
2024-05-27 14:23:51 +0800

    Add BT

--- .專題/one/one.ino
+++ .專題/one/one.ino
@@ -1,20 +1,40 @@
 // 程式庫
 #include <Wire.h> // I2C程式庫
 #include <LiquidCrystal_I2C.h> // LCD_I2C模組程式庫
-
+// BT
+#include <SoftwareSerial.h>
+const byte Txpin=11;
+const byte Rxpin=10;
+SoftwareSerial BT(Rxpin,Txpin); 
+char val;
+// 電壓電流
 #define VT_PIN A2 // connect VT
 #define AT_PIN A3// connect AT
 #define ARDUINO_WORK_VOLTAGE 5.0
-
+// LCD
 LiquidCrystal_I2C lcd(0x27, 20, 2); // LCD I2C位址，默認為0x27或0x3F，依據背板的晶片不同而有差異，16、2為LCD顯示器大小。
 
 void setup() {
+  BT.begin(9600);
   Serial.begin(9600);
+  Serial.println("BT就緒");
   lcd.init();
   lcd.backlight();
 }
 
 void loop() {
+  //BT
+  if (BT.available()){
+    val = BT.read();
+    Serial.print(val);
+  }
+
+  if (Serial.available()){
+    val = Serial.read();
+    Serial.print(val);
+    BT.print(val);
+  }
+  //LCD
   int x = analogRead(A0);
   int y = analogRead(A1);
   int v = analogRead(VT_PIN);
@@ -24,14 +44,17 @@ void loop() {
   double current = a * (ARDUINO_WORK_VOLTAGE / 1023.0);
   w=voltage*current;
   int z=0;
-  z=x-y;
+  int z1=0;
+  z1=x+y;
+  z=z1/2;
+  //z=x-y;
   // Serial顯示
-  Serial.println("X:");
-  Serial.println(x);
-  Serial.println("Y:");
-  Serial.println(y);
-  Serial.println("Z:");
-  Serial.println(z);
+  //Serial.println("X:");
+  //Serial.println(x);
+  //Serial.println("Y:");
+  //Serial.println(y);
+  //Serial.println("Z:");
+  //Serial.println(z);
   // LCD顯示
   lcd.clear();
   lcd.setCursor(0, 0); // (colum, row)從第一排的第三個位置開始顯示
