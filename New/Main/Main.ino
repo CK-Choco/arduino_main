d96ce1c035d22e4d2837230c6341b1e17879372a
HsiaoChoco <avatyu89@gmail.com>
2024-09-12 23:18:04 +0800

    20240912
    23:18

--- New/Main/Main.ino
+++ New/Main/Main.ino
@@ -1,12 +1,3 @@
-/*
- GPIO 0~15不使用
- GPIO 1(TX0) 3(RX0) 藍芽
- GPIO 16~39使用
- GPIO 16 數位輸出
- GPIO 21 22 IIC_I2C數據腳
- GPIO 34 35 36 39 只輸入 (電壓電流)
-*/
-// 數據庫
 #include <BluetoothSerial.h>  // 藍芽
 
 // 藍芽
@@ -21,12 +12,8 @@ BluetoothSerial SerialBT;
 
 void setup() {
   Serial.begin(9600);
-  SerialBT.begin("ESP32_BT"); //藍牙顯示名稱，可自行更改，需避免與他人重複命名
-  Wifi.mode(WIFI_OFF);
-
+  SerialBT.begin("ESP32_BT");
 }
 
 void loop() {
-  SerialBT.write(#輸出內容#);
-  
 }
